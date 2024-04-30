f13eaf7532d39abda54751e566c952a9ee2252af
JaquelineNavarro <155691208+JaquelineNavarro@users.noreply.github.com>
2024-04-29 20:48:32 -0700

    creating the constructor and the object

---
+++ script.js
@@ -0,0 +1,11 @@
+const recipesInformation = [];
+
+const recipe = function (rTitle,rType,rSource,rIngredients,rInstructions,rLink,rNotes) {
+    this.title = rTitle;
+    this.type = rType;
+    this.source = rSource;
+    this.ingredientes = rIngredients;
+    this.instructions = rInstructions;
+    this.link = rLink;
+    this.notes = rNotes;
+}
